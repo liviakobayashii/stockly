7fb1ace1e2584ac3b641bcda16f1ab20f2a4a951
Livia Kobayashi <liviakmmartins@gmail.com>
2026-01-01 15:37:46 -0300

    fix: correction of product data typing

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -19,7 +19,7 @@ const ProductsPage = async () => {
                     Novo Produto
                 </Button>
             </div>
-            <DataTable columns={productTableColumns} data={products} />
+            <DataTable columns={productTableColumns} data={JSON.parse(JSON.stringify(products))} />
         </div>
     )
 }
