c86cc605344cec8b62903f839db31dc7333369e5
Livia Kobayashi <liviakmmartins@gmail.com>
2026-01-09 18:19:32 -0300

    fix: add eslint disable comment for no-var rule in prisma.ts

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-    // eslint-disable-next-line no-var
     var cachedPrisma: ReturnType<typeof createPrismaClient>;
 }
 
